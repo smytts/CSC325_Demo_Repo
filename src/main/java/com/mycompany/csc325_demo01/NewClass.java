1a7cf05fa9682b9d4ba9c979a65a9f842fde9ebf
Timothy <Timothy@MSI_Laptop.lan>
2023-02-04 14:08:23 -0500

    Worked on the NewClass setters, getters, and constructors

--- src/main/java/com/mycompany/csc325_demo01/NewClass.java
+++ src/main/java/com/mycompany/csc325_demo01/NewClass.java
@@ -9,5 +9,22 @@ package com.mycompany.csc325_demo01;
  * @author Timothy
  */
 public class NewClass {
-    
+
+    private int age;
+
+    public NewClass() {
+    }
+
+    public NewClass(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
